b9cc7b4ea5bdf0ea9caf2a3048a9495a55dfd35d
adamit24 <taylorreneeadami@gmail.com>
2022-04-27 08:26:51 -0400

    Create main.py
    Starter template for countdown

---
+++ main.py
@@ -0,0 +1,5 @@
+# Name: [Your Name]
+# Date: 4/24/2022
+# Project: Countdown Timer using Github
+# File: countdownClass --> main.py
+#-----------------------------------------------------------------
